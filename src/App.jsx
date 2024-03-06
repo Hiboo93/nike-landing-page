30ead36dc570e5e4fc92f8fe6a7e41c47e950d1a
Hiboo93 <99762397+Hiboo93@users.noreply.github.com>
2024-03-06 13:31:34 +0100

    📱 F01: configuration design system

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,34 @@
+import Nav from './components/Nav.jsx';
 import { Hero, CustomerReviews, PopularProducts, SpecialOffers, Services, Subscribe, SuperQuality, Footer } from './sections';
 
 export default function App() {
   return (
     <main className="main ">
       <div className="container mx-auto p-4 relative">
-        nav 
+        <Nav/>
         <section className="w-screen">
           <Hero/>
         </section>
-        <section className="w-screen px-8 py-24">
+        <section className="w-screen  py-24">
           <PopularProducts/>
         </section>
-        <section className="w-screen px-8 py-24">
+        <section className="w-screen  py-24">
           <SuperQuality/>
         </section>
-        <section className=" w-screen px-8 py-24">
+        <section className=" w-screen  py-24">
           <Services/>
         </section>
-        <section className=" w-screen px-8 py-24">
+        <section className=" w-screen py-24">
           <SpecialOffers/>
         </section>
-        <section className="bg-blue-50 w-screen px-8 py-24">
+        <section className="bg-blue-50 w-screen py-24">
           <CustomerReviews/>
         </section>
-        <section className="w-screen px-8 py-24">
+        <section className="w-screen py-24">
           <Subscribe/>
         </section>
       </div>
-        <section className="bg-black text-white w-screen px-8 py-24">
+        <section className="bg-black text-white w-screen py-24">
           <Footer/>
         </section>
     </main>
