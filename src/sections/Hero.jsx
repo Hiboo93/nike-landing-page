87ef1f45ae92c56e05b86d5af24f4a16d6419b05
Hiboo93 <99762397+Hiboo93@users.noreply.github.com>
2024-03-06 18:33:23 +0100

    🚧 F02: progress in the Hero component and creation of the button component

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,8 +1,20 @@
+import { arrowRight } from "../assets/icons/index.js"
+import Button from "../components/Button.jsx"
 
 
 const Hero = () => {
   return (
     <section className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container" id="home">
+      <div className="relative xl:w-2/5 flex flex-col justify-items-center items-start w-full max-xl:padding-x pt-28">
+        <p>Our Summer Collection</p>
+        <h1>
+          <span>The New Arrival</span>
+          <br />
+          <span>Nike</span> Shoes
+        </h1>
+        <p>Discover stylish Nike arrivals, quality comfor, and innovation for your active life.</p>
+        <Button label={"Shop now"} iconURL={arrowRight}/>
+      </div>
     </section>
   )
 }
